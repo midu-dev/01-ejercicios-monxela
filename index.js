2572c7d9a219321a34b390d40b066bbc0789a285
Ramón A. Mediavilla <ramon.mediavilla@ironia.tech>
2023-07-27 01:24:39 +0200

    Write file

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+const fs = require('node:fs')
+const path = require('path')
+
 // Ejercicio 2
 async function writeFile (filePath, data, callback) {
+  let directory
+  try {
+    directory = path.dirname(filePath)
+  } catch (exception) {
+    if (exception.name === 'TypeError') {
+      console.log('filePath no es una cadena de texto')
+    } else {
+      console.log(`Se ha producido un error al leer del directorio ${directory}`)
+    }
+    process.exit(1)
+  }
 
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, true)
+  }
+
+  fs.writeFile(filePath, data, function (err) {
+    if (err) {
+      console.log(`No se pudo escribir en el fichero ${filePath}`)
+    } else {
+      callback()
+    }
+  })
 }
 
 // Ejercicio 3
